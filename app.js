4a7e056419e7d6ffa094821262e908d61e1aebca
Aarsh Bajaj <aarshbajaj22@gmail.com>
2025-05-20 22:15:11 -0400

    V2 Using OpenAI AI Model for Images

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const readline = require('readline')
-const {generateMeta} = require('./controllers/openaiController')
+const {generateMeta, generateImage} = require('./controllers/openaiController')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-rl.question('Youtube Video Title: \n', generateMeta)
+// rl.question('Youtube Video Title: \n', generateMeta)
+rl.question('Describe your Youtube Thumbnail \n', generateImage)
 
--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -20,7 +20,23 @@ const generateMeta = async (title) => {
   });
 
   console.log(tags.choices[0].message.content);
-
 };
 
-module.exports = { generateMeta };
+
+const generateImage = async (desc) => {
+  try {
+    // call the correct images endpoint
+    const response = await openai.images.generate({
+      prompt: desc,
+      n: 1,
+      size: '512x512',
+    });
+
+    // the URL lives at response.data[0].url
+    console.log(response.data[0].url);
+  } catch (err) {
+    console.error('Error generating image:', err);
+  }
+};
+
+module.exports = { generateMeta, generateImage };
